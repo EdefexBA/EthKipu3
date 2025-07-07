c3b45bbc451792740fafff3f5fa304e5227a0a15
EdefexBA <96959360+EdefexBA@users.noreply.github.com>
2025-07-06 23:16:50 -0300

    Update Escorpio.sol

--- Escorpio.sol
+++ Escorpio.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Escorpio is ERC20 {
+    constructor() ERC20("Escorpio", "ERP") {
+        _mint(msg.sender, 100000000000 ether);
+    }
+}
